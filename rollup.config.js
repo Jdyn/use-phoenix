0d1af4e81507fa636afaf692fc167165146ba1a7
Jaden <moorejaden5389@gmail.com>
2023-12-17 17:01:12 -0800

    publish 0.0.1-alpha.4

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,45 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import babel from "@rollup/plugin-babel";
-import replace from "@rollup/plugin-replace";
-import terser from "@rollup/plugin-terser";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
 
-import pkg from "./package.json" assert {
-  type: "json"
-};
+import pkg from './package.json' assert { type: 'json' };
 
-const isDev = process.env.BUILD !== "production";
+const isDev = process.env.BUILD !== 'production';
 
 const cjs = {
-  file: pkg.main,
-  format: "cjs",
-  exports: "named",
-  sourcemap: true,
-  plugins: !isDev && [terser()],
+	file: pkg.main,
+	format: 'cjs',
+	exports: 'named',
+	sourcemap: true,
+	plugins: !isDev && [terser()]
 };
 
 const esm = {
-  file: pkg.module,
-  format: "esm",
-  exports: "named",
-  sourcemap: true,
+	file: pkg.module,
+	format: 'esm',
+	exports: 'named',
+	sourcemap: true
 };
 
-const extensions = [".js", ".ts", ".tsx", ".json"];
+const extensions = ['.js', '.ts', '.tsx', '.json'];
+
 const plugins = [
-  resolve({ extensions }),
-  commonjs(),
-  babel({ exclude: "node_modules/**", extensions }),
-  replace({
-    preventAssignment: true,
-    "process.env.NODE_ENV": JSON.stringify(
-      isDev ? "development" : "production"
-    ),
-  }),
+  typescript(),
+	resolve({ extensions }),
+	commonjs(),
+	babel({ exclude: 'node_modules/**', extensions }),
+	replace({
+		'preventAssignment': true,
+		'process.env.NODE_ENV': JSON.stringify(isDev ? 'development' : 'production')
+	})
 ].filter(Boolean);
 
 export default {
-  input: "index.ts",
-  output: isDev ? [esm] : [cjs, esm],
-  plugins,
-  external: Object.keys(pkg.peerDependencies),
+	input: 'index.ts',
+	output: isDev ? [esm] : [cjs, esm],
+	plugins,
+	external: Object.keys(pkg.peerDependencies)
 };
