2481302ad68ea91de129b0f33653a363a1afb174
Jaden <moorejaden5389@gmail.com>
2024-01-05 22:49:21 -0800

    fix losing reference to channel if it was lost inbetween setStates (#5)

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,5 @@
 import { PhoenixSocket } from './usePhoenix';
+import type { Channel } from 'phoenix';
 
-export const findChannel = (socket: PhoenixSocket, topic: string) =>
-	socket.channels.find((channel) => channel.topic === topic);
+export const findChannel = (socket: PhoenixSocket | null, topic: string): Channel | undefined =>
+	socket ? socket.channels.find((channel) => channel.topic === topic) : undefined;
